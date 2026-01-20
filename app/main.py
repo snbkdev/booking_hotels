cf44407f93c7353785c40a9e97782c7ad312e63a
Asanbek09 <samudinua@gmail.com>
2026-01-20 09:39:24 +0600

    Валидация данных с Pydantic

--- app/main.py
+++ app/main.py
@@ -1,11 +1,54 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Depends
 from typing import Optional
 from datetime import date
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/hotels")
-def get_hotels(location: str, date_from: date, date_to: date, stars: Optional[int] = Query(None, ge=1, le=5), has_spa: Optional[bool] = None):
-    return location, date_from, date_to
+class HotelsSearchArgs:
+    def __init__(
+        self,
+        location: str, 
+        date_from: date, 
+        date_to: date, 
+        stars: Optional[int] = Query(None, ge=1, le=5), 
+        has_spa: Optional[bool] = None
+        ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.stars = stars
+        self.has_spa = has_spa
 
-#LED0903MOW14041
+class SHotel(BaseModel):
+    address: str
+    name: str
+    stars: int
+
+@app.get("/")
+def get_hotels(location: str, 
+               date_from: date, 
+               date_to: date, 
+               stars: Optional[int] = Query(None, ge=1, le=5), 
+               has_spa: Optional[bool] = None) -> list[SHotel]:
+    hotels = [
+        {
+            "address": "ул. Советскаяб 37, Бишкек",
+            "name": "Hayyat",
+            "stars": 4,
+        },
+    ]
+    return hotels
+
+@app.get("/hotels")
+def search_hotels(search_args: HotelsSearchArgs = Depends()):
+    return search_args
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+@app.post("/bookings")
+def add_booking(booking: SBooking):
+    pass
