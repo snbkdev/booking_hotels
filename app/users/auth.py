3caf2a5df1792d0ffe58a87870b2a6add612db0f
Asanbek09 <samudinua@gmail.com>
2026-01-25 17:08:09 +0600

    Регистрация пользователя по почте

---
+++ app/users/auth.py
@@ -0,0 +1,18 @@
+from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=60)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, "asdasdqweqasd", "HS256")
+    return encoded_jwt
